10dd20bb20e4483dc9ba2e72255cc0e74047969a
jun.okubo1994 <junokuborocks@gmail.com>
2022-02-27 21:55:44 +0900

    Made some buttons work

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ function reveal() {
     }
   }
 
+  var cv=document.getElementById('cv');
+  console.log(cv);
+// function downloading(){
+//   // alert('Downloading CV...');
+//   console.log('workgin')
+// }
+
+
+  cv.addEventListener("click", function(){
+  
+      alert('Downloading CV...');
+  })
   // window.addEventListener("scroll", reveal);
 
   // document.onreadystatechange = function () {
